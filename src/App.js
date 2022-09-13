7031fa9ab99941be6cc2fd4c66e97f1c9734ea15
ibktommy <atomquartz@gmail.com>
2022-09-13 10:52:35 +0100

    Setting up component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react'
+import Photo from './component/Photo'
 import { FaSearch } from 'react-icons/fa'
-import Photo from './Photo'
+
 // const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+// APP API URL
 const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
