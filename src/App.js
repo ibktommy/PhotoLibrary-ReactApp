324e697c2a190fc3d03ae9c9b3d60d7da11a331c
ibktommy <atomquartz@gmail.com>
2022-09-13 11:45:48 +0100

    Set App-States, create Function that fetches data from API-URL, calll the function using the useEffect-Hook

--- src/App.js
+++ src/App.js
@@ -8,6 +8,31 @@ const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
 function App() {
+  // App State 
+  const [isLoading, setIsLoading] = useState(false)
+
+  const [photos, setPhotos] = useState([])
+
+  // Setting Up FUnction to Fetch Photos Data
+  const fetchPhotos = async () => {
+    setIsLoading(true)
+    let url;
+    url = `${mainUrl}${clientID}`
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data)
+
+    } catch (error) {
+      setIsLoading(false)
+      console.log(error.message)
+    }
+  }
+
+  // Setting Up useEffect Hook to Call The FetchPhotos function
+  useEffect(() => {
+    fetchPhotos()
+  }, [])
   return <h2>stock photos starter</h2>
 }
 
