ff1f4eea1f926a8dd55f8cfe28376455bf0aed98
ibktommy <atomquartz@gmail.com>
2022-09-13 11:32:23 +0100

    Setting up access keys

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Photo from './component/Photo'
 import { FaSearch } from 'react-icons/fa'
 
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const clientID = `?client_id=${process.env.REACT_APP_API_ACCESS_KEY}`
 // APP API URL
 const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
